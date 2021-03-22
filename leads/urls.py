871729932e2fd4d11e32e04eab71ee6b1142e223
Aditya Gupta <adityagupta.ad62001@gmail.com>
2021-03-22 21:55:16 +0530

    Changed hard code of urls and used namespace method

--- leads/urls.py
+++ leads/urls.py
@@ -3,9 +3,9 @@ from .views import lead_list,lead_detail,lead_create,lead_update,lead_delete
 
 app_name='leads'
 urlpatterns=[
-    path('',lead_list),
-    path('<int:pk>/',lead_detail),#Added int: so that pk indentifies only integer as id
-    path('create/',lead_create),
-    path('<int:pk>/update/',lead_update),
-    path('<int:pk>/delete/',lead_delete)
+    path('',lead_list,name='lead-list'),
+    path('<int:pk>/',lead_detail,name='lead-detail'),#Added int: so that pk indentifies only integer as id
+    path('create/',lead_create,name='lead-create'),
+    path('<int:pk>/update/',lead_update,name='lead-update'),
+    path('<int:pk>/delete/',lead_delete,name='lead-delete')
     ]
