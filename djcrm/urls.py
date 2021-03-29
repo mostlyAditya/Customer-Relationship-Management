f1f5be68cc37e35371368e0b7e779a3c8ce4274d
Aditya Gupta <adityagupta.ad62001@gmail.com>
2021-03-29 23:59:55 +0530

    Added Logout button

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path,include
 from leads.views import landing_page,LandingPageView
 
@@ -25,7 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/',include('leads.urls',namespace='leads')),
     path('',LandingPageView.as_view(),name="landing-page"), #path('', landing_page,name="landing-page")
-    path('login/',LoginView.as_view(),name='login')
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout')
 ]
 
 if settings.DEBUG:
